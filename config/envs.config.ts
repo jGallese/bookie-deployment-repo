4a4a6f3491be02937ba3d2c8ae6b29305a345bbe
Mariela Reziale <marielareziale@iMac-de-Mariela.local>
2024-06-18 23:55:30 -0300

    feat (conections) add envs config and ready to deploy

---
+++ config/envs.config.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+import { get } from 'env-var';
+
+export const envs = {
+  MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+  MONGO_URL: get('MONGO_URL').required().asString(),
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,13 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { envs } from 'config/envs.config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://bookiedb:bookiedb@bookie-db.7cxcdeh.mongodb.net/?retryWrites=true&w=majority&appName=Bookie-DB',
-      {
-        dbName: 'Bookie-DB',
-      },
-    ),
+    MongooseModule.forRoot(envs.MONGO_URL, {
+      dbName: envs.MONGO_DB_NAME,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
