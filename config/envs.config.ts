c2c91f912d1c0f810d85d232d797046ac4377ef6
JuaniMolina48 <juanirojo10@gmail.com>
2024-06-24 17:15:46 -0300

    fix(env): fix merge env config

--- config/envs.config.ts
+++ config/envs.config.ts
@@ -4,8 +4,7 @@ import { get } from 'env-var';
 export const envs = {
   MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
   MONGO_URL: get('MONGO_URL').required().asString(),
-  GOOGLE_BOOKS_APIKEY: get('GOOGLE_BOOKS_APIKEY').required().asString()
+  GOOGLE_BOOKS_APIKEY: get('GOOGLE_BOOKS_APIKEY').required().asString(),
   JWT_SECRET: get('JWT_SECRET').required().asString(),
   JWT_EXPIRES_IN: get('JWT_EXPIRES_IN').required().asString(),
- 
 };
